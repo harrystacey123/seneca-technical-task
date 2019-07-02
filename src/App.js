3767686d83a55cb3b9919d9d4baa74abb3020ab6
harrystacey123 <harrystacey@live.co.uk>
2019-07-02 10:24:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Options from './Options';
+
+let question = {
+  heading: 'An Animal Cell Contains:',
+  answer1: {
+    wrongAnswer: 'Cell Wall',
+    correctAnswer: 'Ribosomes'
+  },
+  answer2: {
+    wrongAnswer: 'Chloroplast',
+    correctAnswer: 'Cytoplasm'
+  },
+  answer3: {
+    wrongAnswer: 'Impermeable membrane',
+    correctAnswer: 'Partially permeable membrane'
+  },
+  answer4: {
+    wrongAnswer: 'Cellulose',
+    correctAnswer: 'Mitochondrea'
+  }
+}
+
+let questions = [question, question, question, question, question];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{question.heading}</h2>
+      <Options />
     </div>
   );
 }
